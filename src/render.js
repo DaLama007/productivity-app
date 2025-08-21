e59c3d265a4ee2cfa1ad34d2ceb428d1a6f53a26
DaLama007 <marwan.lamesch@gmail.com>
2025-08-21 22:50:04 +0100

    Fix UI, and make slider and dropdown to set time/mode. Start making store.

--- src/render.js
+++ src/render.js
@@ -1,3 +1,4 @@
+
 let lastWindowId = 0
 let counter = 0
 timerPaused = localStorage.setItem('timerPaused', 'true');
@@ -19,18 +20,23 @@ setInterval(() => {
     });
 }, 500)
 percentage = 100
-setTime=10000
+setTime = 10000
 time = 10000
 onePercentInTime = time / percentage
 let timerInterval = null;
-function startTimer() {
-  localStorage.setItem('time-left', time);
-  seconds = time / 1000;
+
+//convert ms to right time format
+function msToTime(ms){
+  seconds = ms / 1000;
   hours = Math.floor(seconds / 3600);
   minutes = Math.floor((seconds % 3600) / 60);
   seconds = Math.floor(seconds % 60);
-  document.getElementById('number').textContent = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
+}
+function startTimer() {
+  localStorage.setItem('time-left', time);
+  document.getElementById('number').textContent = msToTime(time);
   if (time > 0) {
     percentage = time / onePercentInTime;
     const circumference = 2 * Math.PI * 140
@@ -39,9 +45,9 @@ function startTimer() {
     document.documentElement.style.setProperty('--offset', offset);
     time -= 100
   }
-  else if(time==0){
+  else if (time == 0) {
     window.electronAPI.sendNotification("Distraction Blocker", "Timer has ended");
-    time=setTime;
+    time = setTime;
     onClickTimer();
   }
 
@@ -49,40 +55,54 @@ function startTimer() {
 
 function onClickTimer() {
   timerPaused = localStorage.getItem('timerPaused');
-  let PSButtonE=document.getElementById('PS-Button');
-  if (timerPaused=='true'){
-    timerPaused='false';
-    PSButtonE.textContent='Pause';
+  let PSButtonE = document.getElementById('PS-Button');
+  if (timerPaused == 'true') {
+    timerPaused = 'false';
+    PSButtonE.textContent = 'Pause';
     PSButtonE.classList.add('paused');
   }
-  else{
-    timerPaused='true';
-    PSButtonE.textContent='Continue';
+  else {
+    timerPaused = 'true';
+    PSButtonE.textContent = 'Continue';
     PSButtonE.classList.remove('paused');
   }
   localStorage.setItem('timerPaused', timerPaused);
-  
-  if (timerPaused=='false') {
+
+  if (timerPaused == 'false') {
     timerInterval = setInterval(() => {
       startTimer();
     }, 100);
   }
-  else{
+  else {
     clearInterval(timerInterval);
   }
 }
 
 /*collapse sidebar and change body*/
-function collapseElement(element){
+function collapseElement(element) {
   element.classList.toggle("collapsed");
   document.body.classList.toggle("collapsed");
   console.log("collapsed sidebar")
 }
-sidebar=document.getElementById("sidebar");
+//shrink and expand sidebar if user hovers over it or out
+sidebar = document.getElementById("sidebar");
 sidebar.addEventListener("mouseenter", () => {
-    collapseElement(sidebar)
-  });
+  collapseElement(sidebar)
+});
 sidebar.addEventListener("mouseleave", () => {
-    collapseElement(sidebar)
-  });
+  collapseElement(sidebar)
+});
+
+//slider value listener and getter
+slider=document.getElementById("time-slider");
+slider.addEventListener("change",()=>{
+  displaySliderValue(slider.value);
+});
+//slider value display
+function displaySliderValue(value){
+  sliderValue=document.getElementById("valueDisplay");
+  sliderValue.textContent=msToTime(slider.value);
+  console.log(value);
+}
+
 
